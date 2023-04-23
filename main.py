3f228128279e4401be104148ac88f2665fefdf6f
Anton <omelchenko230783@gmail.com>
2023-04-23 11:41:40 +0200

    final revision

--- main.py
+++ main.py
@@ -22,12 +22,12 @@ def get_birthdays_per_week(users: list) -> None:            # This function give
     for diction in users:
         while diction:
             key,value = diction.popitem()
-            birthday_now_year = datetime(datetime.now().year, value.month, value.day).date() # here we get birthday of user with this year
-            week_num = birthday_now_year.weekday() # number of weekday from 0 to 6
-            weekday= birthday_now_year.strftime('%A') # here we get name of the day of the week
-            if end >= birthday_now_year >= start and week_num < 5: # we check if this date meets with requirements
-                result[weekday].append(key) # we write the date to the result
-            elif end >= birthday_now_year >= start and week_num >= 5: # we check if this date meets with requirements
-                result['Monday'].append(key) # we write the date to the result
+            birthday_now_year = datetime(datetime.now().year, value.month, value.day).date()            # here we get birthday of user with this year
+            week_num = birthday_now_year.weekday()          # number of weekday from 0 to 6
+            weekday= birthday_now_year.strftime('%A')               # here we get name of the day of the week
+            if end >= birthday_now_year >= start and week_num < 5:          # we check if this date meets with requirements
+                result[weekday].append(key)             # we write the date to the result
+            elif end >= birthday_now_year >= start and week_num >= 5:           # we check if this date meets with requirements
+                result['Monday'].append(key)            # we write the date to the result
     for key, value in result.items():
-        print(key, ', '.join(value), sep=': ') # we print the result to console
+        print(key, ', '.join(value), sep=': ')          # we print the result to console
