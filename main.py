81f7271bc3ada1d577f32a7663b66c8c156cbc5c
Anton <omelchenko230783@gmail.com>
2023-04-25 19:35:14 +0200

    final revision

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 from datetime import datetime, timedelta
 
 
-result = {          # result what would be printed after executing the function
-    'Monday': [],
-    'Tuesday': [],
-    'Wednesday': [],
-    'Thursday': [],
-    'Friday': []
-}
-
-
 def period_of_check() -> list:          # after executing this function we get period from saturday to friday next week
     date_now = datetime.now().date()
     start = date_now - timedelta(date_now.weekday()) + timedelta(days=5)
@@ -18,6 +9,7 @@ def period_of_check() -> list:          # after executing this function we get p
 
 
 def get_birthdays_per_week(users: list) -> None:            # This function give list of users with weekdays of their birthdays
+    result = {}
     start, end = period_of_check()
     for diction in users:
         while diction:
@@ -26,8 +18,12 @@ def get_birthdays_per_week(users: list) -> None:            # This function give
             week_num = birthday_now_year.weekday()          # number of weekday from 0 to 6
             weekday= birthday_now_year.strftime('%A')               # here we get name of the day of the week
             if end >= birthday_now_year >= start and week_num < 5:          # we check if this date meets with requirements
-                result[weekday].append(key)             # we write the date to the result
+                if weekday in result:
+                    result[weekday].append(key)             # we write the date to the result
+                else: result[weekday] = []; result[weekday].append(key)
             elif end >= birthday_now_year >= start and week_num >= 5:           # we check if this date meets with requirements
-                result['Monday'].append(key)            # we write the date to the result
+                if weekday in result or 'Monday' in result:
+                    result['Monday'].append(key)            # we write the date to the result
+                else: result['Monday'] = []; result['Monday'].append(key)
     for key, value in result.items():
         print(key, ', '.join(value), sep=': ')          # we print the result to console
